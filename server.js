4bfbb77bb880d07d7cfc711da205c0a6c310e517
Jaime Rodas <jaime@surrealista.mx>
2016-03-15 23:12:26 -0600

    Empezamos
    Hicimos un filtro súper básico para los usuarios
    Inicializamos / que apunte a /public

---
+++ server.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var usuarios = require('./users.json');
+
+app.set('port', (process.env.PORT || 3000));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+function search(term) {
+  return function(usuario) {
+    var nombreCompleto = usuario.nombre + ' ' + usuario.apellido;
+    return (nombreCompleto.toLowerCase().indexOf(term.toLowerCase()) > -1);
+  };
+}
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.post('/search', function(req, res) {
+  if (req.body.q.length < 2) {
+    res.json([]).end();
+  }
+
+  res.json(usuarios.filter(search(req.body.q))).end();
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
